bc2786856eb2c33018a8172148e36477ad3aebdb
Frankley Francalino da Rocha <frankley.rocha@tse.jus.br>
2025-04-04 15:50:53 -0300

    ajustes

--- phpapp/admin/index.php
+++ phpapp/admin/index.php
@@ -1,8 +1,8 @@
 <?php
 echo "Hello, Admin!\n";
 
-$username = $_SERVER['HTTP_X_CONSUMER_USERNAME'] ?? 'desconhecido';
-echo "Usuário: " . htmlspecialchars($username) . "\n";
+$username = $_SERVER['HTTP_X_CONSUMER_USERNAME'] ?? 'unknown';
+echo "User: " . htmlspecialchars($username) . "\n";
 
-$groups = $_SERVER['HTTP_X_CONSUMER_GROUPS'] ?? 'nenhum grupo';
-echo "Grupos do consumidor: " . htmlspecialchars($groups) . "\n";
+$groups = $_SERVER['HTTP_X_CONSUMER_GROUPS'] ?? 'no group';
+echo "Groups: " . htmlspecialchars($groups) . "\n";
